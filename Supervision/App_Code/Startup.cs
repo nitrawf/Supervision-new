f1c134ac1e72b6443e8013c23d92ad75ee79a840
darezik <ddarezik@gmail.com>
2018-01-22 18:24:09 +0530

    Add project files.

---
+++ Supervision/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Supervision.Startup))]
+namespace Supervision
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
